7e5798f731b049b19ac3703a51a6ee466700f3b0
NextSap <nextsap@outlook.com>
2024-05-23 10:59:26 +0200

    fix anti spam bug

--- lib/action/check-spam.ts
+++ lib/action/check-spam.ts
@@ -17,5 +17,5 @@ export async function checkSpam(email: string, phone: string) {
 
     if(!ticket) return false;
 
-    return ticket.creationDate.getDate() < (Date.now() + 60000);
+    return ticket.creationDate.getMilliseconds() < (Date.now() + 30000);
 }
